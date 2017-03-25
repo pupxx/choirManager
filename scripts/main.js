f312bdc56259ff92b747cecabab116622a2e38d1
Cheryln <cherylnbarber@gmail.com>
2017-03-25 14:18:51 -0700

    refactored code on main js page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,41 @@
 console.log('This is looking great Cheryln!');
+// ==================== Functions =====================
+// ====================================================
 
+function appendDetails (idOrClassStr, WrapperStr, name, email, phone){
+  $(idOrClassStr).on('click', function(){
+    $(WrapperStr).children().remove();
+    $(WrapperStr).append(`
+      <div class="row">
+        <div class="col-lg-10 col-lg-offset-2">
+          <h4>${name}</h4>
+          <h6>Email: <a>${email}</a></h6>
+          <h6>Phone: ${phone}</h6>
+        </div>
+      </div>`);
+  });
+}
+
+function appendQuote (arr){
+  $('document').ready(function(){
+    var quoteObj = arr[Math.floor(Math.random() * arr.length)];
+    $('#quoteWrapper').append(`<div id="quote">${quoteObj.quote}</div>`);
+    $('#quoteWrapper').append(`<div id="reference">${quoteObj.reference}</div>`);
+  });
+}
+
+// ====================  Code ==============================
+// =========================================================
 
 var quotes =
 [
   {quote: 'If thou art merry, praise the Lord with singing, with music, with dancing, and with a prayer of praise and thanksgiving.', reference: '~Doctrine and Covenants 136:28~'}, {quote: 'Sing unto the Lord; for he hath done excellent things: this is known in all the earth.', reference: '~Isaiah 12:5~'}, {quote: 'Sing unto the Lord, O ye saints of his, and give thanks at the remembrance of his holiness.', reference: '~Psalm 30:4~'}
 ];
 
-$('document').ready(function(){
+appendQuote (quotes);
 
-  var quoteObj = quotes[Math.floor(Math.random() * quotes.length)];
-    $('#quoteWrapper').append(`<div id="quote">${quoteObj.quote}</div>`);
-    $('#quoteWrapper').append(`<div id="reference">${quoteObj.reference}</div>`);
-});
+appendDetails ('#director', '#leadershipDetailsWrapper', 'Jean Barber', 'ejeanbarber@gmail.com', '705-435-7974');
 
-$('#director').on('click', function(){
-  $('#leadershipDetailsWrapper').children().remove();
-  $('#leadershipDetailsWrapper').append(`
-    <div class="row">
-      <div class="col-lg-10 col-lg-offset-2">
-        <h4>Jean Barber</h4>
-        <h6>Email: <a>ejeanbarber@gmail.com</a></h6>
-        <h6>Phone: 705-435-7974</h6>
-      </div>
-    </div>`);
-});
+appendDetails ('#chairman', '#leadershipDetailsWrapper', 'Carol Smith', 'carolrdsmith@gmail.com', '705-424-9452');
 
-$('#chairman').on('click', function(){
-  $('#leadershipDetailsWrapper').children().remove();
-  $('#leadershipDetailsWrapper').append(`
-    <div class="row">
-      <div class="col-lg-10 col-lg-offset-2">
-        <h4>Carol Smith</h4>
-        <h6>Email: <a>carolrdsmith@gmail.com</a></h6>
-        <h6>Phone: 705-424-9452</h6>
-      </div>
-    </div>`);
-});
-
-$('#president').on('click', function(){
-  $('#leadershipDetailsWrapper').children().remove();
-  $('#leadershipDetailsWrapper').append(`
-    <div class="row">
-      <div class="col-lg-10 col-lg-offset-2">
-        <h4>Carol Smith</h4>
-        <h6>Email: <a>carolrdsmith@gmail.com</a></h6>
-        <h6>Phone: 705-424-9452</h6>
-      </div>
-    </div>`);
-});
+appendDetails ('#accompanist', '#leadershipDetailsWrapper', 'Carol Smith', 'carolrdsmith@gmail.com', '705-424-9452');
